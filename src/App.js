69e23f6770bd98b604755942bd4f53c5d9c877c0
Angelia Horne <AngeliaMHorne@gmail.com>
2023-04-24 02:53:46 -0600

    oops

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import React from 'react'
 import Home from './Home'
+import Nav from './Nav'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+      <Nav />
+      <Home />
        <main>
         <Routes>
           <Route path="/" exact element={<Home />} />
