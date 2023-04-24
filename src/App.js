68aa2b73559937eae25bf8532bb69168d3fc9516
Angelia Horne <AngeliaMHorne@gmail.com>
2023-04-24 03:01:21 -0600

    adding

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react'
 import Home from './Home'
+import About from './About'
 import Nav from './Nav'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -9,11 +10,10 @@ function App() {
     <BrowserRouter>
       <div className="App">
       <Nav />
-      <Home />
        <main>
         <Routes>
           <Route path="/" exact element={<Home />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<About />} />
         </Routes>
       </main>
       </div>
