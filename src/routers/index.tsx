441b0584b4d06ee7699dad0e7ececb88eecf20dc
Angelia Horne <AngeliaMHorne@gmail.com>
2023-04-24 19:40:18 -0600

    tfeven

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { AppHome, AngeliaHome } from "../pages/Home";
+import { AppHome, AngeliaHome } from "../pages";
 
 export const AppRouter: FC = () => {
     return (
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -19,11 +19,11 @@ export const getApp = () => {
 const getSubdomain = (location: string) => {
     const locationParts = location.split(".");
 
-    let sliceTill = -3
+    let sliceTill = -2
 
 //for localhost
-const isMainHost = locationParts.slice(-3)[0] === "www";
-if (isMainHost) sliceTill = -3
+const isMainHost = locationParts.slice(-2)[0] === "www";
+if (isMainHost) sliceTill = -2
 
         return locationParts.slice(0, sliceTill).join("");
 
