658553c6447da681e0fa1ec99f5a3ac0ebe67613
Angelia Horne <AngeliaMHorne@gmail.com>
2023-04-24 18:52:20 -0600

    error

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import React = require("react");
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { AppHome, AngeliaHome } from "../pages/Home";
 
